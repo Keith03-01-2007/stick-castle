7cf92db2025b009cdba6304597a453b139638e04
Keith03-01-2007 <62977560+Keith03-01-2007@users.noreply.github.com>
2020-05-18 19:07:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(800,400);
+  var rect = createSprite(400,300,60,200);
+  var rect = createSprite(525,300,60,200);
+  var rect = createSprite(275,300,60,200);
+  var rect = createSprite(400,180,190,40);
+  var rect = createSprite(400,160,130,40);
+  var rect = createSprite(400,140,60,40);
+  var rect = createSprite(400,375,400,50);
+   var rect = createSprite(400,100,6,40);
+  var rect = createSprite(420,90,40,20);
+ var rect = createSprite(150,250,20,300);
+ var rect = createSprite(650,250,20,300);
+ 
+ }
+function draw(){ 
+  background("black");
+  
+  drawSprites();}
